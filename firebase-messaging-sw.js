6a1c50345990d7a57e7f3cb1fe6d0dd40e91a4fc
Howard Sun <howard@funtek.co>
2024-04-19 02:11:28 +0000

    GitLab CI/CD Deploy artifacts to GitHub repo

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 importScripts(
-  'https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js'
+  'https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js'
 )
 importScripts(
-  'https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js'
+  'https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js'
 )
 
 firebase.initializeApp({
